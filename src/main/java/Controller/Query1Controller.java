f7dec4fd0a2c0ba4e5dee2e12bbfd33b63878e5c
aswindle <aswindle@gmail.com>
2018-11-30 16:26:18 -0700

    Query 1 now working

--- src/main/java/Controller/Query1Controller.java
+++ src/main/java/Controller/Query1Controller.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,11 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import Model.Query1;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 public class Query1Controller {
@@ -26,8 +32,44 @@ public class Query1Controller {
     }
 
     @GetMapping("/query1")
-    public String addpharmacistForm(Model model) {
+    public String query1Form(Model model) {
         model.addAttribute("query1", new Query1());
         return "query1";
+    }
+
+    @PostMapping("/query1")
+    public String appointmentAdd(Model model, @ModelAttribute Query1 query) {
+        List<String> allNames = this.jdbcTemplate.query(
+                "SELECT p.pid, p.f_name, p.l_name, p.gender, p.dob, a.appt_date, a.reason, a.treatment, d.f_name AS " +
+                        "d_f, d.l_name AS d_l\n" +
+                        "FROM aswindle.patient p, aswindle.appointment a, aswindle.doctor d\n" +
+                        "WHERE p.l_name = ?\n" +
+                        "AND p.f_name = ?\n" +
+                        "AND p.dob = ?\n" +
+                        "AND p.pid = a.pid\n" +
+                        "AND a.did = d.did\n" +
+                        "AND ROWNUM = 1\n" +
+                        "ORDER BY a.appt_date DESC", new RowMapper<String>() {
+                    public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        int pid = rs.getInt("pid");
+                        String first_name = rs.getString("f_name");
+                        String last_name = rs.getString("l_name");
+                        String gender = rs.getString("gender");
+                        // TODO: switch date longs back to human-readable dates
+                        Date dob = new Date(rs.getLong("dob"));
+                        Date appt_date = new Date(rs.getLong("appt_date"));
+                        String reason = rs.getString("reason");
+                        String treatment = rs.getString("treatment");
+                        String d_f = rs.getString("d_f");
+                        String d_l = rs.getString("d_l");
+                        return (String.format("Patient ID: %d\nName: %s %s\nGender: %s\nDOB: %s\nLast Appointment: %s\n" +
+                                        "Reason for appointment: %s\nTreatment: %s\nDoctor: %s %s",
+                                pid, first_name, last_name, gender, dob, appt_date, reason, treatment, d_f, d_l));
+                    }
+                }, query.getlName(), query.getfName(), query.getDOB().getTime());
+
+
+        model.addAttribute("names", allNames);
+        return "resultQuery1";
     }
 }
